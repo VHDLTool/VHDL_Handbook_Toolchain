415bbda6126bb047ad623cc9d7ecbfd75c526cb3
mannif <mannif@PC-2206808.cnesnet.ad.cnes.fr>
2015-12-01 08:05:32 +0100

    replace  std 05500 examples

--- src/Extras/VHDL/STD_05500_bad.vhd
+++ src/Extras/VHDL/STD_05500_bad.vhd
@@ -1,16 +1,16 @@
 -------------------------------------------------------------------------------------------------
 -- Company   : CNES
 -- Author    : Mickael Carl (CNES)
--- Copyright : Copyright (c) CNES. 
+-- Copyright : Copyright (c) CNES.
 -- Licensing : GNU GPLv3
 -------------------------------------------------------------------------------------------------
 -- Version         : V1
--- Version history : 
+-- Version history :
 --    V1 : 2015-04-10 : Mickael Carl (CNES): Creation
 -------------------------------------------------------------------------------------------------
 -- File name          : STD_05500_bad.vhd
 -- File Creation date : 2015-04-10
--- Project name       : VHDL Handbook CNES Edition 
+-- Project name       : VHDL Handbook CNES Edition
 -------------------------------------------------------------------------------------------------
 -- Softwares             :  Microsoft Windows (Windows 7) - Editor (Eclipse + VEditor)
 -------------------------------------------------------------------------------------------------
@@ -22,14 +22,14 @@
 --               This example is compliant with the Handbook version 1.
 --
 -------------------------------------------------------------------------------------------------
--- Naming conventions: 
+-- Naming conventions:
 --
 -- i_Port: Input entity port
 -- o_Port: Output entity port
 -- b_Port: Bidirectional entity port
 -- g_My_Generic: Generic entity port
 --
--- c_My_Constant: Constant definition 
+-- c_My_Constant: Constant definition
 -- t_My_Type: Custom type definition
 --
 -- My_Signal_n: Active low signal
@@ -51,19 +51,19 @@ use IEEE.numeric_std.all;
 
 --CODE
 entity STD_05500_bad is
-   port  (
-      i_Enable    : in std_logic;   -- Enable signal
-      i_Reset_n   : in std_logic;   -- Reset signal
-      i_D         : in std_logic;   -- Latch input signal
-      o_Q         : out std_logic   -- Latch output signal
-   );
+   port (
+      i_Enable  : in  std_logic;        -- Enable signal
+      i_Reset_n : in  std_logic;        -- Reset signal
+      i_D       : in  std_logic;        -- Latch input signal
+      o_Q       : out std_logic         -- Latch output signal
+      );
 end STD_05500_bad;
 
 architecture Behavioral of STD_05500_bad is
-   signal Q : std_logic;   -- Latch output
+   signal Q : std_logic;                -- Latch output
 begin
    -- Asynchronous memory latch
-   Q <= i_D when i_Enable='1' else Q; --/!\ i_Enable is a common signal (not a clock)
+   Q   <= i_D when i_Enable = '1' else Q;  --/!\ i_Enable is a common signal (not a clock)
    o_Q <= Q;
 end Behavioral;
 --CODE
